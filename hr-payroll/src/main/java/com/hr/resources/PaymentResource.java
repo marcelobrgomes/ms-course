a8cfba622c1ecd3e5869c4eb9a83a22e717fe2ed
Marcelo de Britto Gomes <marcelo.gomes@conexasaude.com.br>
2021-01-22 19:03:57 -0300

    Implementação mockada do ms-payroll

---
+++ hr-payroll/src/main/java/com/hr/resources/PaymentResource.java
@@ -0,0 +1,24 @@
+package com.hr.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hr.entities.Payment;
+import com.hr.services.PaymentService;
+
+@RestController
+@RequestMapping("/payments")
+public class PaymentResource {
+
+	@Autowired
+	private PaymentService paymentService;
+	
+	@GetMapping("/{workerId}/days/{days}")
+	public ResponseEntity<Payment> getPayment(@PathVariable long workerId, @PathVariable Integer days) {
+		return ResponseEntity.ok(paymentService.getPayment(workerId, days));
+	}
+}
