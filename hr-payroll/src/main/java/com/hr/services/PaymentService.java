78ea71903a4857cec95922e6fc87033ee888d448
Marcelo de Britto Gomes <marcelo.gomes@conexasaude.com.br>
2021-01-26 11:48:05 -0300

    RestTemplate

--- hr-payroll/src/main/java/com/hr/services/PaymentService.java
+++ hr-payroll/src/main/java/com/hr/services/PaymentService.java
@@ -1,13 +1,30 @@
 package com.hr.services;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.hr.entities.Payment;
+import com.hr.entities.Worker;
 
 @Service
 public class PaymentService {
+	
+	@Value("${hr-worker.host}")
+	private String workerHost;
+	
+	@Autowired
+	private RestTemplate restTemplate;
 
 	public Payment getPayment(long workerId, int days) {
-		return new Payment("Bob", 100.12, days);
+		Map<String, String> variables = new HashMap<String, String>();
+		variables.put("id", String.valueOf(workerId));
+		
+		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, variables);
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 }
