f4bb2d8ba3ea6589e306a88694847031f9b35f86
Marcelo de Britto Gomes <marcelo.gomes@conexasaude.com.br>
2021-02-23 18:57:26 -0300

    Login, jwt token

--- hr-oauth/src/main/java/com/hr/oauth/resources/UserResource.java
+++ hr-oauth/src/main/java/com/hr/oauth/resources/UserResource.java
@@ -3,6 +3,7 @@ package com.hr.oauth.resources;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,9 +20,9 @@ public class UserResource {
 	private UserService service;
 	
 	@GetMapping("/search")
-	private ResponseEntity<User> findByEmail(@RequestParam String email) {
+	private ResponseEntity<UserDetails> findByEmail(@RequestParam String email) {
 		try {
-			User user = service.findByEmail(email);
+			UserDetails user = service.loadUserByUsername(email);
 			return ResponseEntity.ok(user);
 		} catch(IllegalArgumentException e) {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
